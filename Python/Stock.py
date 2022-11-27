bb29738e3c01aee263ef64801e7cda47e2f3e33d
Joseph <jpatrick5402@gmail.com>
2022-11-27 15:55:59 -0500

    API implementation complete

--- Python/Stock.py
+++ Python/Stock.py
@@ -2,19 +2,26 @@ import time
 import json
 import requests
 import copy
+import os
+
+KEY = os.environ["FKEY"]
 
 def getOldJson():
-    with open("Stocks.json", "r") as f:
+    with open("./Stocks.json", "r") as f:
         return json.load(f)
 
 def getNewJson(oldjson):
     newjson = copy.copy(oldjson)
     for i in oldjson:
-        newjson[i] = oldjson[i] + 1 #Replce with API code
+        URL = f"https://api.twelvedata.com/price?symbol={i}&apikey={KEY}"
+
+        price = requests.get(URL).json()["price"]
+
+        newjson[i] = float(price)
     return newjson
 
 def updateJson(newjson):
-    with open("Stocks.json", "w") as f:
+    with open("./Stocks.json", "w") as f:
         json.dump(newjson, f)
 
 def main():
@@ -23,7 +30,7 @@ def main():
     print(f"Old: {oldjson}")
     print(f"New: {newjson}")
     for i in oldjson:
-        if int(newjson[i]) > int(oldjson[i]):
+        if newjson[i] > oldjson[i]:
             print(f"Buy {i}")
         else:
             print(f"Don't Buy {i}")
