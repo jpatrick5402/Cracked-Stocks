d9915970898e615d55795cfa414d7cd46e51db52
Joseph <jpatrick5402@gmail.com>
2022-11-27 16:38:55 -0500

    Preparing for Alpacas implementation

--- Python/Stock.py
+++ Python/Stock.py
@@ -1,16 +1,16 @@
+import datetime
 import time
 import json
 import requests
 import copy
 import os
 
-KEY = os.environ["FKEY"]
 
 def getOldJson():
     with open("./Stocks.json", "r") as f:
         return json.load(f)
 
-def getNewJson(oldjson):
+def getNewJson(oldjson, KEY):
     newjson = copy.copy(oldjson)
     for i in oldjson:
         URL = f"https://api.twelvedata.com/price?symbol={i}&apikey={KEY}"
@@ -24,18 +24,30 @@ def updateJson(newjson):
     with open("./Stocks.json", "w") as f:
         json.dump(newjson, f)
 
-def main():
-    oldjson = getOldJson()
-    newjson = getNewJson(oldjson)
-    print(f"Old: {oldjson}")
-    print(f"New: {newjson}")
-    for i in oldjson:
-        if newjson[i] > oldjson[i]:
-            print(f"Buy {i}")
-        else:
-            print(f"Don't Buy {i}")
-    updateJson(newjson)
+def Calculate(oldjson,newjson):
     pass
+
+def main():
+
+    while(True):
+        try:
+            KEY = os.environ["FKEY"]
+        except:
+            KEY = input("Enter your API KEY: ")
+        oldjson = getOldJson()
+        newjson = getNewJson(oldjson, KEY)
+        print(datetime.datetime.now())
+        print(f"Old: {oldjson}")
+        print(f"New: {newjson}")
+        for i in oldjson:
+            if newjson[i] > oldjson[i] + 2:
+                print(f"Buy {i}")
+            else:
+                print(f"Don't Buy {i}")
+        updateJson(newjson)
+        time.sleep(7*24*60*60)
+    
+    return 0
 
 if __name__ == "__main__":
     main()
