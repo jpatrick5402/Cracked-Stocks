6a3e3270745828fd51566bb623f29573f5e30aca
Joseph <jpatrick5402@gmail.com>
2022-11-27 23:53:13 -0500

    Swithced to Alpaca for Newjson

--- Python/Stock.py
+++ Python/Stock.py
@@ -1,23 +1,18 @@
-import datetime
-import time
-import json
-import requests
-import copy
-import os
-
+import datetime, time, json, copy, os
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockLatestBarRequest
 
 def getOldJson():
     with open("./Stocks.json", "r") as f:
         return json.load(f)
 
-def getNewJson(oldjson, KEY):
+def getNewJson(oldjson, KEY, PKEY):
     newjson = copy.copy(oldjson)
+    client = StockHistoricalDataClient(KEY, PKEY)
     for i in oldjson:
-        URL = f"https://api.twelvedata.com/price?symbol={i}&apikey={KEY}"
-
-        price = requests.get(URL).json()["price"]
-
-        newjson[i] = float(price)
+            requests = StockLatestBarRequest(symbol_or_symbols=i)
+            data = client.get_stock_latest_bar(request_params=requests)
+            newjson[i] = data[i].dict()["close"]
     return newjson
 
 def updateJson(newjson):
@@ -27,15 +22,21 @@ def updateJson(newjson):
 def Calculate(oldjson,newjson):
     pass
 
+def Invest(stock): 
+    pass
+
 def main():
 
     while(True):
         try:
-            KEY = os.environ["FKEY"]
+            KEY = os.environ["API_KEY"]
+            PKEY = os.environ["PRIVATE_KEY"]
         except:
             KEY = input("Enter your API KEY: ")
+            PKEY = input("Enter your Private API KEY: ")
+
         oldjson = getOldJson()
-        newjson = getNewJson(oldjson, KEY)
+        newjson = getNewJson(oldjson, KEY, PKEY)
         print(datetime.datetime.now())
         print(f"Old: {oldjson}")
         print(f"New: {newjson}")
